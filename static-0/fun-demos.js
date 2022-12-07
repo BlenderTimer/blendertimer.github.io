b9e020eeb69964b55c1deb4e1e7783212943842d
Daniel Roberts <118873082+BlenderTimer@users.noreply.github.com>
2022-12-06 20:39:44 -0800

    Add files via upload

--- static-0/fun-demos.js
+++ static-0/fun-demos.js
@@ -14,40 +14,79 @@ var uranusDTMiles = document.getElementById("uranus-dt-mi");
 var uranusDTKilometers = document.getElementById("uranus-dt-km");
 var neptuneDTMiles = document.getElementById("neptune-dt-mi");
 var neptuneDTKilometers = document.getElementById("neptune-dt-km");
+
+var ceresDTMiles = document.getElementById("ceres-dt-mi");
+var ceresDTKilometers = document.getElementById("ceres-dt-km");
+var plutoDTMiles = document.getElementById("pluto-dt-mi");
+var plutoDTKilometers = document.getElementById("pluto-dt-km");
+var haumeaDTMiles = document.getElementById("haumea-dt-mi");
+var haumeaDTKilometers = document.getElementById("haumea-dt-km");
+var makemakeDTMiles = document.getElementById("makemake-dt-mi");
+var makemakeDTKilometers = document.getElementById("makemake-dt-km");
+var erisDTMiles = document.getElementById("eris-dt-mi");
+var erisDTKilometers = document.getElementById("eris-dt-km");
+
+var sunDTMiles = document.getElementById("sun-dt-mi");
+var sunDTKilometers = document.getElementById("sun-dt-km");
+
 var initialTime = new Date().getTime();
 
 function setPlanetDistance(planet) {
 	if (planet == "mercury") {
 		mercuryDTMiles.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 29.4) + " mi";
-		mercuryDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 47.3) + " km";
+		mercuryDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 29.4 * 1.609199471538894) + " km";
 	}
 	else if (planet == "venus") {
 		venusDTMiles.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 21.8) + " mi";
-		venusDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 35.1) + " km";
+		venusDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 21.8 * 1.609199471538894) + " km";
 	}
 	else if (planet == "earth") {
 		earthDTMiles.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 18.5) + " mi";
-		earthDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 29.8) + " km";
+		earthDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 18.5 * 1.609199471538894) + " km";
 	}
 	else if (planet == "mars") {
 		marsDTMiles.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 15.0) + " mi";
-		marsDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 24.1) + " km";
+		marsDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 15.0 * 1.609199471538894) + " km";
 	}
 	else if (planet == "jupiter") {
 		jupiterDTMiles.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 8.1) + " mi";
-		jupiterDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 13.0) + " km";
+		jupiterDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 8.1 * 1.609199471538894) + " km";
 	}
 	else if (planet == "saturn") {
 		saturnDTMiles.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 6.0) + " mi";
-		saturnDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 9.7) + " km";
+		saturnDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 6.0 * 1.609199471538894) + " km";
 	}
 	else if (planet == "uranus") {
 		uranusDTMiles.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 4.2) + " mi";
-		uranusDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 6.8) + " km";
+		uranusDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 4.2 * 1.609199471538894) + " km";
 	}
 	else if (planet == "neptune") {
 		neptuneDTMiles.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 3.4) + " mi";
-		neptuneDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 5.5) + " km";
+		neptuneDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 3.4 * 1.609199471538894) + " km";
+	}
+	else if (planet == "ceres") {
+		ceresDTMiles.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 11.11235) + " mi";
+		ceresDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 11.11235 * 1.609199471538894) + " km";
+	}
+	else if (planet == "pluto") {
+		plutoDTMiles.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 2.945563) + " mi";
+		plutoDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 2.945563 * 1.609199471538894) + " km";
+	}
+	else if (planet == "haumea") {
+		haumeaDTMiles.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 2.786478) + " mi";
+		haumeaDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 2.786478 * 1.609199471538894) + " km";
+	}
+	else if (planet == "makemake") {
+		makemakeDTMiles.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 2.746085) + " mi";
+		makemakeDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 2.746085 * 1.609199471538894) + " km";
+	}
+	else if (planet == "eris") {
+		erisDTMiles.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 2.135222) + " mi";
+		erisDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 2.135222 * 1.609199471538894) + " km";
+	}
+	else if (planet == "sun") {
+		sunDTMiles.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 142.9282) + " mi";
+		sunDTKilometers.innerHTML = Math.round(((new Date().getTime() - initialTime) / 1000) * 142.9282 * 1.609199471538894) + " km";
 	}
 }
 
@@ -60,4 +99,10 @@ function planetsTravelDistance() {
 	setInterval(function() {setPlanetDistance("saturn")},30);
 	setInterval(function() {setPlanetDistance("uranus")},30);
 	setInterval(function() {setPlanetDistance("neptune")},30);
+	setInterval(function() {setPlanetDistance("ceres")},30);
+	setInterval(function() {setPlanetDistance("pluto")},30);
+	setInterval(function() {setPlanetDistance("haumea")},30);
+	setInterval(function() {setPlanetDistance("makemake")},30);
+	setInterval(function() {setPlanetDistance("eris")},30);
+	setInterval(function() {setPlanetDistance("sun")},30);
 }
