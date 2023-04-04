fb177592abcfedbe16d558b7b631c909a5b13bc9
Daniel Roberts <118873082+BlenderTimer@users.noreply.github.com>
2023-04-03 22:10:55 -0700

    Add files via upload

--- static-0/general.js
+++ static-0/general.js
@@ -3,10 +3,13 @@ var warningBanner = document.getElementById('warning-banner');
 var smallMenu = document.getElementById('small-menu');
 var largeMenu = document.getElementById('large-menu');
 var largeMenuItem = document.getElementsByClassName('large-menu-item');
+var largeMenuItemDD = document.getElementsByClassName('large-menu-itemDD');
+var ulul = document.querySelectorAll('nav ul li ul');
 var smallMenuMain = document.getElementById('small-menu-main');
 var contentMain = document.getElementById('content-main');
 var navDropdown = document.all["nav-dropdown"]
 var smallMenuOpen = false
+var ululHeights = [];
 
 window.onscroll = function(){
 	bgPlate.style.backgroundPosition = "0px " + (document.scrollingElement.scrollTop * -0.15) + "px";
@@ -19,19 +22,41 @@ function hideWarningBanner(){
 }
 
 function determineDisplayMenu() {
-	if (window.innerWidth < 1000){
+	if (window.innerWidth < 700) {
 		smallMenu.style.display = "block";
 		for (var i=0; i < largeMenuItem.length; i++) {
   			largeMenuItem[i].style.display = "none";
 		}
+		for (var i=0; i < largeMenuItemDD.length; i++) {
+  			largeMenuItemDD[i].style.display = "none";
+		}
 		smallMenuMain.style.display = "block";
+		contentMain.style.margin = "50px 0px 0px 0px";
+		contentMain.style.borderRadius = "0px";
 	}
-	else{
+	else if (window.innerWidth < 1000) {
+		smallMenu.style.display = "block";
+		for (var i=0; i < largeMenuItem.length; i++) {
+  			largeMenuItem[i].style.display = "none";
+		}
+		for (var i=0; i < largeMenuItemDD.length; i++) {
+  			largeMenuItemDD[i].style.display = "none";
+		}
+		smallMenuMain.style.display = "block";
+		contentMain.style.margin = "50px 5vw 50px 5vw";
+		contentMain.style.borderRadius = "10px";
+	}
+	else {
 		smallMenu.style.display = "none";
 		for (var i=0; i < largeMenuItem.length; i++) {
   			largeMenuItem[i].style.display = "block";
 		}
+		for (var i=0; i < largeMenuItemDD.length; i++) {
+  			largeMenuItemDD[i].style.display = "block";
+		}
 		smallMenuMain.style.display = "none";
+		contentMain.style.margin = "50px 5vw 50px 5vw";
+		contentMain.style.borderRadius = "10px";
 	}
 }
 
@@ -42,6 +67,18 @@ function baseLoad() {
 	warningBanner.style.margin = "0px 0px 0px 0px";
 	warningBanner.style.opacity = "1";
 	bgPlate.style.backgroundPosition = "0px " + (document.scrollingElement.scrollTop * -0.15) + "px";
+	for (var i=0; i < ulul.length; i++) {
+		ululHeights.push(ulul[i].offsetHeight);
+	}
+}
+
+function setululHeight(ululId, action) {
+	if (action == 'enter') {
+		ulul[ululId].style.height = ululHeights[ululId];
+	}
+	else {
+		ulul[ululId].style.height = 0;
+	}
 }
 
 function smallMenuToggle() {
@@ -49,12 +86,14 @@ function smallMenuToggle() {
 		smallMenuMain.style.boxShadow = "0px 20px 20px rgba(0,0,0,0.0)";
 		smallMenuMain.style.width = "0px";
 		smallMenuMain.style.opacity = "0";
+		smallMenu.style.background = "#1E1E1E";
 		smallMenuOpen = false;
 	}
 	else{
 		smallMenuMain.style.boxShadow = "0px 20px 20px rgba(0,0,0,0.5)";
 		smallMenuMain.style.width = "200px";
 		smallMenuMain.style.opacity = "1";
+		smallMenu.style.background = "#2E2E2E";
 		smallMenuOpen = true;
 	}
 }
