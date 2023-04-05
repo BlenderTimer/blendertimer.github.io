05d82f1a43de0ac89fac2e909d78e5121226c449
Daniel Roberts <118873082+BlenderTimer@users.noreply.github.com>
2023-04-05 12:32:14 -0700

    Add files via upload

---
+++ static-0/channel-overview.js
@@ -0,0 +1,34 @@
+var totalViews = document.getElementById("total-views");
+var totalSubscribers = document.getElementById("total-subscribers");
+var totalVideos = document.getElementById("total-videos");
+
+function determineLayout() {
+    if (window.innerWidth < 700) {
+        totalViews.style.fontSize = ((window.innerWidth / 30) + 16.5).toString() + "pt";
+        totalSubscribers.style.fontSize = ((window.innerWidth / 30) + 16.5).toString() + "pt";
+        totalVideos.style.fontSize = ((window.innerWidth / 30) + 16.5).toString() + "pt";
+    }
+	else {
+		totalViews.style.fontSize = "40pt";
+		totalSubscribers.style.fontSize = "40pt";
+		totalVideos.style.fontSize = "40pt";
+	}
+}
+
+$.get(
+    "https://www.googleapis.com/youtube/v3/channels",{
+    part: 'statistics',
+    id: 'UCFMTkXP-dTa3cBh8XxVKBGQ',
+    key: 'AIzaSyDsaGX5mhvqCpI3jWMFFGMmmmB5IUEyUrk'},
+   	function(data) {
+       	var viewC = 0;
+       	var subC = 0;
+       	var vidC = 0;
+       	viewC = parseInt(data.items[0].statistics.viewCount);
+       	subC = parseInt(data.items[0].statistics.subscriberCount);
+       	vidC = parseInt(data.items[0].statistics.videoCount);
+		totalViews.innerHTML = viewC.toLocaleString("en-US")
+		totalSubscribers.innerHTML = subC.toLocaleString("en-US")
+		totalVideos.innerHTML = vidC.toLocaleString("en-US")
+	}
+);
