8330e6a7e0e07f37838f22be917120ca9cb45341
Daniel Roberts <118873082+BlenderTimer@users.noreply.github.com>
2023-08-13 22:15:41 -0700

    Add files via upload

--- static-0/about.js
+++ static-0/about.js
@@ -11,12 +11,29 @@ var funStats = document.getElementById('fun-stats');
 var leftElements = document.getElementById('left-elements');
 var rightElements = document.getElementById('right-elements');
 var topspr = document.getElementById('topspr');
+var closeXButton = document.getElementById('close-x-button');
+var contactPopupDiv = document.getElementById('contact-popup-div');
+var popupBackground = document.getElementById('popup-background');
+var cpdTitle = document.getElementById('cpd-title');
+var cpdText = document.getElementById('cpd-text');
 
 function init() {
 	if (window.location.toString().indexOf("#contact") > -1) {
 		setTimeout(function() {contactMain.style.background = "#B02525"}, 50);
 		setTimeout(function() {contactMain.style.background = "#252525"}, 2000);
 	}
+	var cxbc = closeXButton.getContext("2d");
+	cxbc.beginPath();
+	cxbc.moveTo(10, 10);
+	cxbc.lineTo(20, 20);
+	cxbc.strokeStyle = "#555555";
+	cxbc.lineWidth = 2;
+	cxbc.lineCap = "round";
+	cxbc.stroke();
+	cxbc.beginPath();
+	cxbc.moveTo(20, 10);
+	cxbc.lineTo(10, 20);
+	cxbc.stroke();
 }
 
 function contactLinkToggle() {
@@ -38,7 +55,7 @@ function contactLinkToggle() {
 }
 
 function contactLinkMouseLeave() {
-	contactLink.style.background = "#252525";
+	contactLink.style.background = "#25252500";
 	contactLink.style.boxShadow = "none";
 	contactLink.style.width = "100%";
 	contactLink.style.marginLeft = "0px";
@@ -72,6 +89,10 @@ function determineLayout() {
 		rightElements.style.width = "100%";
 		rightElements.style.marginTop = "15px";
 		topspr.style.display = "block";
+		contactPopupDiv.style.left = "20px";
+		contactPopupDiv.style.right = "20px";
+		contactPopupDiv.style.top = "20px";
+		contactPopupDiv.style.bottom = "20px";
 	}
 	else if (window.innerWidth < 1000) {
 		faq.style.width = "100%";
@@ -82,6 +103,10 @@ function determineLayout() {
 		rightElements.style.width = "calc(100% - 256px - 30px)";
 		rightElements.style.marginTop = "0px";
 		topspr.style.display = "none";
+		contactPopupDiv.style.left = "10vw";
+		contactPopupDiv.style.right = "10vw";
+		contactPopupDiv.style.top = "10vh";
+		contactPopupDiv.style.bottom = "10vh";
 	}
 	else {
 		faq.style.width = "calc(100% - 256px - 30px)";
@@ -92,5 +117,38 @@ function determineLayout() {
 		rightElements.style.width = "calc(100% - 256px - 30px)";
 		rightElements.style.marginTop = "0px";
 		topspr.style.display = "none";
+		contactPopupDiv.style.left = "10vw";
+		contactPopupDiv.style.right = "10vw";
+		contactPopupDiv.style.top = "10vh";
+		contactPopupDiv.style.bottom = "10vh";
 	}
+}
+
+function resizeB() {
+    contactPopupDiv.style.top = (window.innerHeight / 2) - (contactPopupDiv.offsetHeight / 2);
+    contactPopupDiv.style.left = (window.innerWidth / 2) - (contactPopupDiv.offsetWidth / 2);
+}
+
+function contactPopup(title) {	
+    cpdTitle.textContent = title;
+	setTimeout(function() {contactPopupDiv.style.opacity = "1"; popupBackground.style.opacity = "1"}, 1);
+	contactPopupDiv.style.display = "block";
+	popupBackground.style.display = "block";
+	popupBackground.style.bottom = 0;
+}
+
+function popupCloseBegin(enter) {
+	if (enter == true) {
+		closeXButton.style.background = "#2E2E2E";
+		closeXButton.style.boxShadow = "0px 0px 10px rgba(0,0,0,0.5)";
+	}
+	else {
+		closeXButton.style.background = "#1E1E1E";
+		closeXButton.style.boxShadow = "none";
+	}
+}
+function popupClose() {
+	contactPopupDiv.style.opacity = "0";
+	popupBackground.style.opacity = "0";
+	setTimeout(function() {contactPopupDiv.style.display = "none"; popupBackground.style.display = "none"}, 500);
 }
