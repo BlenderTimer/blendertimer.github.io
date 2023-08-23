dac82c4fc302429b26268ac1947007867c193122
Daniel Roberts <118873082+BlenderTimer@users.noreply.github.com>
2023-08-22 20:33:07 -0700

    Add files via upload

--- static-0/general.js
+++ static-0/general.js
@@ -11,6 +11,7 @@ var navDropdown = document.all["nav-dropdown"]
 var smallMenuOpen = false
 var ululHeights = [];
 var revolveButton = document.getElementById('revolve-button');
+var navLinksWidths = 0;
 
 window.onscroll = function(){
 	bgPlate.style.backgroundPosition = "0px " + (document.scrollingElement.scrollTop * -0.15) + "px";
@@ -23,7 +24,7 @@ function hideWarningBanner(){
 }
 
 function determineDisplayMenu() {
-	if (window.innerWidth < 700) {
+	if (window.innerWidth < (navLinksWidths + (window.innerWidth / 10) + 298)) {
 		smallMenu.style.display = "block";
 		for (var i=0; i < largeMenuItem.length; i++) {
   			largeMenuItem[i].style.display = "none";
@@ -32,21 +33,14 @@ function determineDisplayMenu() {
   			largeMenuItemDD[i].style.display = "none";
 		}
 		smallMenuMain.style.display = "block";
-		contentMain.style.margin = "50px 0px 0px 0px";
-		contentMain.style.borderRadius = "0px";
-		revolveButton.style.marginRight = "50px";
-	}
-	else if (window.innerWidth < 1000) {
-		smallMenu.style.display = "block";
-		for (var i=0; i < largeMenuItem.length; i++) {
-  			largeMenuItem[i].style.display = "none";
+		if (window.innerWidth < 700) {
+			contentMain.style.margin = "50px 0px 0px 0px";
+			contentMain.style.borderRadius = "0px";
 		}
-		for (var i=0; i < largeMenuItemDD.length; i++) {
-  			largeMenuItemDD[i].style.display = "none";
-		}
-		smallMenuMain.style.display = "block";
-		contentMain.style.margin = "50px 5vw 50px 5vw";
-		contentMain.style.borderRadius = "10px";
+		else {
+			contentMain.style.margin = "50px 5vw 50px 5vw";
+			contentMain.style.borderRadius = "10px";
+		};
 		revolveButton.style.marginRight = "50px";
 	}
 	else {
@@ -65,7 +59,8 @@ function determineDisplayMenu() {
 }
 
 function baseLoad() {
-	determineDisplayMenu()
+	loadMenu();
+	determineDisplayMenu();
 	contentMain.style.marginLeft = "5%";
 	contentMain.style.opacity = "1";
 	warningBanner.style.margin = "0px 0px 0px 0px";
@@ -75,6 +70,85 @@ function baseLoad() {
 		ululHeights.push(ulul[i].offsetHeight);
 		ulul[i].style.height = "0px";
 	}
+}
+
+function loadMenu() {
+	for (var i=0; i < navLinks.length; i++) {
+		var daysSinceAdded = (new Date().getTime() - navLinks[i].date.getTime()) / 1000 / 60 / 60 / 24;
+		var smallLink = document.createElement('a');
+		smallLink.innerHTML = navLinks[i].title;
+		smallLink.className = "small-menu-button-0";
+		if (navLinks[i].url.length > 0) {
+			smallLink.href = navLinks[i].url;
+		};
+		smallMenuMain.appendChild(smallLink);
+		var largeLink = document.createElement('li');
+		largeLink.id = i.toString();
+		if (navLinks[i].dropdownURLs) {
+			largeLink.className = "large-menu-itemDD";
+		}
+		else {
+			largeLink.className = "large-menu-item";
+		};
+		largeLink.style.display = "none";
+		largeLink.addEventListener("mouseenter", function(event) {
+			var element = event.target || event.srcElement;
+			setululHeight(element.id, 'enter')
+		});
+		largeLink.addEventListener("mouseleave", function(event) {
+			var element = event.target || event.srcElement;
+			setululHeight(element.id, 'leave')
+		});
+		if (daysSinceAdded < 30) {
+			var largeLinkNew = document.createElement('p');
+			largeLinkNew.innerHTML = "NEW";
+			largeLinkNew.className = "large-menu-new";
+			largeLink.appendChild(largeLinkNew);
+			var smallLinkNew = document.createElement('p');
+			smallLinkNew.innerHTML = "NEW";
+			smallLinkNew.className = "small-menu-new";
+			smallLink.appendChild(smallLinkNew);
+		}
+		var largeLinkA = document.createElement('a');
+		largeLinkA.innerHTML = navLinks[i].title;
+		largeLinkA.style.width = navLinks[i].width;
+		if (navLinks[i].url.length > 0) {
+			largeLinkA.href = navLinks[i].url;
+		};
+		largeLink.appendChild(largeLinkA);
+		var largeLinkUL = document.createElement('ul');
+		largeLinkUL.style.width = navLinks[i].width;
+		if (navLinks[i].dropdownURLs) {
+			for (var i2=0; i2 < navLinks[i].dropdownURLs.length; i2++) {
+				var daysSinceAddedDD = (new Date().getTime() - navLinks[i].dropdownURLs[i2].date.getTime()) / 1000 / 60 / 60 / 24;
+				var smallLink2 = document.createElement('a');
+				smallLink2.innerHTML = navLinks[i].dropdownURLs[i2].title;
+				smallLink2.className = "small-menu-button-1";
+				smallLink2.href = navLinks[i].dropdownURLs[i2].url;
+				smallMenuMain.appendChild(smallLink2);
+				var largeLinkLI = document.createElement('li');
+				if (daysSinceAddedDD < 30) {
+					var largeLinkNewDD = document.createElement('p');
+					largeLinkNewDD.innerHTML = "";
+					largeLinkNewDD.className = "large-menu-newDD";
+					largeLinkLI.appendChild(largeLinkNewDD);
+					var smallLinkNew2 = document.createElement('p');
+					smallLinkNew2.innerHTML = "";
+					smallLinkNew2.className = "small-menu-new-1";
+					smallLink2.appendChild(smallLinkNew2);
+				}
+				var largeLinkLIA = document.createElement('a');
+				largeLinkLIA.innerHTML = navLinks[i].dropdownURLs[i2].title
+				largeLinkLIA.href = navLinks[i].dropdownURLs[i2].url
+				largeLinkLI.appendChild(largeLinkLIA);
+				largeLinkUL.appendChild(largeLinkLI);
+			};
+		};
+		largeLink.appendChild(largeLinkUL);
+		largeMenu.appendChild(largeLink);
+		navLinksWidths += navLinks[i].width + 10;
+	};
+	ulul = document.querySelectorAll('nav ul li ul');
 }
 
 function setululHeight(ululId, action) {
@@ -96,7 +170,7 @@ function smallMenuToggle() {
 	}
 	else{
 		smallMenuMain.style.boxShadow = "0px 20px 20px rgba(0,0,0,0.5)";
-		smallMenuMain.style.width = "200px";
+		smallMenuMain.style.width = "250px";
 		smallMenuMain.style.opacity = "1";
 		smallMenu.style.background = "#303030";
 		smallMenuOpen = true;
---
+++ static-0/nav-links.js
@@ -0,0 +1,77 @@
+var navLinks = [
+	{
+		title:"YouTube Channel",
+		url:"https://www.youtube.com/BlenderTimer",
+		width:170,
+		date:new Date(2023, 3, 1), //year, month(from 0), day
+		dropdownURLs:[
+			{
+				title:"Overview",
+				url:"./channel",
+				date:new Date(2022, 4, 7), //year, month(from 0), day
+			},
+			{
+				title:"Video Sources",
+				url:"./video-sources",
+				date:new Date(2023, 3, 1), //year, month(from 0), day
+			},
+			{
+				title:"Timeline",
+				url:"./channel-timeline",
+				date:new Date(2022, 11, 4), //year, month(from 0), day
+			},
+			{
+				title:"Submit a video idea!",
+				url:"./submit-a-video-idea",
+				date:new Date(2022, 11, 4), //year, month(from 0), day
+			},
+		],
+	},
+	{
+		title:"Resources",
+		url:"",
+		width:140,
+		date:new Date(2023, 0, 11), //year, month(from 0), day
+		dropdownURLs:[
+			{
+				title:"Factsheets",
+				url:"./factsheets",
+				date:new Date(2022, 11, 25), //year, month(from 0), day
+			},
+			{
+				title:"Fun Demos",
+				url:"./fun-demos",
+				date:new Date(2022, 11, 4), //year, month(from 0), day
+			},
+			{
+				title:"Web Tools",
+				url:"./web-tools",
+				date:new Date(2023, 0, 11), //year, month(from 0), day
+			},
+		],
+	},
+	{
+		title:"Shop",
+		url:"https://shop.blendertimer.com",
+		width:90,
+		date:new Date(2023, 7, 21), //year, month(from 0), day
+	},
+	{
+		title:"About",
+		url:"./about",
+		width:110,
+		date:new Date(2023, 3, 1), //year, month(from 0), day
+		dropdownURLs:[
+			{
+				title:"FAQs",
+				url:"./about#faq",
+				date:new Date(2023, 3, 1), //year, month(from 0), day
+			},
+			{
+				title:"Contact",
+				url:"./about#contact",
+				date:new Date(2022, 11, 4), //year, month(from 0), day
+			},
+		],
+	},
+];
