f51af0c390ea8a7b93df411d14d4fdc56531df09
Daniel Roberts <118873082+BlenderTimer@users.noreply.github.com>
2024-01-21 13:43:24 -0800

    Add files via upload

---
+++ programming-elements/programming-elements.js
@@ -0,0 +1,35 @@
+var elementBlocks = document.getElementsByClassName("element-block");
+
+function determineLayout() {
+	if (window.innerWidth < 500) {
+		for (var i=0; i < elementBlocks.length; i++) {
+  			elementBlocks[i].style.width = "100%";
+		}
+	}
+	else if (window.innerWidth < 830) {
+		for (var i=0; i < elementBlocks.length; i++) {
+  			elementBlocks[i].style.width = "calc(50% - 20px)";
+		}
+	}
+	else if (window.innerWidth < 1100) {
+		for (var i=0; i < elementBlocks.length; i++) {
+  			elementBlocks[i].style.width = "calc(33.33333% - 20px)";
+		}
+	}
+	else if (window.innerWidth < 1700) {
+		for (var i=0; i < elementBlocks.length; i++) {
+  			elementBlocks[i].style.width = "calc(25% - 20px)";
+		}
+	}
+	else if (window.innerWidth < 2100) {
+		for (var i=0; i < elementBlocks.length; i++) {
+  			elementBlocks[i].style.width = "calc(20% - 20px)";
+		}
+	}
+	else {
+		for (var i=0; i < elementBlocks.length; i++) {
+  			elementBlocks[i].style.width = "calc(16.66666% - 20px)";
+		}
+	}
+	determineDisplayMenu();
+}
