30f2b50c0c13baff36c0e8caff77c0c780f16201
Daniel Roberts <118873082+BlenderTimer@users.noreply.github.com>
2024-01-21 13:58:10 -0800

    Add files via upload

--- static-0/nav-links.js
+++ static-0/nav-links.js
@@ -1,28 +1,28 @@
 var navLinks = [
 	{
 		title:"YouTube Channel",
-		url:"./channel",
+		url:"/channel",
 		width:170,
 		date:new Date(2023, 10, 17), //year, month(from 0), day
 		dropdownURLs:[
 			{
 				title:"Overview",
-				url:"./channel-overview",
+				url:"/channel-overview",
 				date:new Date(2023, 10, 17), //year, month(from 0), day
 			},
 			{
 				title:"Video Sources",
-				url:"./video-sources",
+				url:"/video-sources",
 				date:new Date(2023, 3, 1), //year, month(from 0), day
 			},
 			{
 				title:"Milestones",
-				url:"./channel-milestones",
+				url:"/channel-milestones",
 				date:new Date(2023, 8, 6), //year, month(from 0), day
 			},
 			{
 				title:"Submit a video idea!",
-				url:"./submit-a-video-idea",
+				url:"/submit-a-video-idea",
 				date:new Date(2022, 11, 4), //year, month(from 0), day
 			},
 		],
@@ -35,27 +35,27 @@ var navLinks = [
 		dropdownURLs:[
 			{
 				title:"Factsheets",
-				url:"./factsheets",
+				url:"/factsheets",
 				date:new Date(2022, 11, 25), //year, month(from 0), day
 			},
 			{
 				title:"Fun Demos",
-				url:"./fun-demos",
+				url:"/fun-demos",
 				date:new Date(2022, 11, 4), //year, month(from 0), day
 			},
 			{
 				title:"Web Tools",
-				url:"./web-tools",
+				url:"/web-tools",
 				date:new Date(2023, 8, 3), //year, month(from 0), day
 			},
 			{
 				title:"Programming Elements",
-				url:"./programming-elements",
+				url:"/programming-elements",
 				date:new Date(2024, 0, 21), //year, month(from 0), day
 			},
 			{
 				title:"BT-JS",
-				url:"./bt-js",
+				url:"/bt-js",
 				date:new Date(2024, 0, 20), //year, month(from 0), day
 			},
 		],
@@ -68,23 +68,23 @@ var navLinks = [
 	},
 	{
 		title:"About",
-		url:"./about",
+		url:"/about",
 		width:120,
 		date:new Date(2024, 0, 7), //year, month(from 0), day
 		dropdownURLs:[
 			{
 				title:"FAQs",
-				url:"./about#faq",
+				url:"/about#faq",
 				date:new Date(2023, 3, 1), //year, month(from 0), day
 			},
 			{
 				title:"Contact",
-				url:"./about#contact",
+				url:"/about#contact",
 				date:new Date(2022, 11, 4), //year, month(from 0), day
 			},
 			{
 				title:"Privacy Policy",
-				url:"./terms/privacy-policy",
+				url:"/terms/privacy-policy",
 				date:new Date(2024, 0, 7), //year, month(from 0), day
 			},
 		],
