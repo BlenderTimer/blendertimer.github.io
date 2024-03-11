d704726d7b30420770f7e204a02505f461e96d43
Daniel Roberts <118873082+BlenderTimer@users.noreply.github.com>
2024-03-10 20:41:52 -0700

    Add files via upload

--- static-0/bt-js.js
+++ static-0/bt-js.js
@@ -1,4 +1,4 @@
-document.getElementById('download').innerHTML = "Download BT-JS " + btjsVersion;
+document.getElementById('btjs-download').innerHTML = "Download BT-JS " + btjsVersion;
 document.getElementById('change-log').innerHTML = btjsVersion + " Change Log";
 
 function link(l) {
