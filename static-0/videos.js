864ed1cc746e7da8b33179b323b0aa476ad136f0
Daniel Roberts <blendertimer.d@gmail.com>
2025-01-04 17:36:05 -0800

    Add files via upload

--- static-0/videos.js
+++ static-0/videos.js
@@ -391,4 +391,18 @@ const videos = [
 		views1: 475,
 		views24: 3700,
 	},
+	{
+		name:"Some Really BIG Numbers - Fantastic Fact 12",
+		tui:29,
+		sui:12,
+		type:"short",
+		url:"29-some-really-big-numbers",
+		cc:"1 thousand has 0 sets of 3 zeros succeeding the thousand...obviously. 1 million has 1 set of 3 zeros succeeding the thousand. And continuing up the list we have billion, trillion, quadrillion, quintillion, sextillion, septillion, octillion, nonillion, decillion, undecillion, duodecillion, tredecillion, quattuordecillion, quindecillion, sexdecillion, septendecillion, octodecillion, novemdecillion, vigintillion, unvigintillion, duovigintillion, trevigintillion, quattuorvigintillion, quinvigintillion, sexvigintillion, septenvigintillion, octovigintillion, novemvigintillion, trigintillion, untrigintillion, duotrigintillion, googol, and finally googolplex. This isn't even a googolplex, it's just a white rectangle because a googolplex literally exceeds the number of atoms in the Observable Universe.",
+		youtubeID: "hoYLF1GGEcA",
+		date: "4-1-2025",
+		views10: -1,
+		views30: -1,
+		views1: -1,
+		views24: -1,
+	},
 ];
