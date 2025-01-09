e52867db3e2a71270bd38a3cee1e97cb9c8888ed
Daniel Roberts <blendertimer.d@gmail.com>
2025-01-09 12:42:17 -0800

    Add files via upload

--- static-0/channel-overview.js
+++ static-0/channel-overview.js
@@ -2,19 +2,6 @@ var totalViews = document.getElementById("total-views");
 var totalSubscribers = document.getElementById("total-subscribers");
 var totalVideos = document.getElementById("total-videos");
 
-function determineLayout() {
-    if (window.innerWidth < 700) {
-        totalViews.style.fontSize = ((window.innerWidth / 30) + 16.5).toString() + "pt";
-        totalSubscribers.style.fontSize = ((window.innerWidth / 30) + 16.5).toString() + "pt";
-        totalVideos.style.fontSize = ((window.innerWidth / 30) + 16.5).toString() + "pt";
-    }
-	else {
-		totalViews.style.fontSize = "40pt";
-		totalSubscribers.style.fontSize = "40pt";
-		totalVideos.style.fontSize = "40pt";
-	}
-}
-
 $.get(
     "https://www.googleapis.com/youtube/v3/channels",{
     part: 'statistics',
