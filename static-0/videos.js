a717ebffa44ea8406fc61b04e4a2f028425483f4
Daniel Roberts <blendertimer.d@gmail.com>
2025-07-16 12:14:08 -0700

    Add files via upload

--- static-0/videos.js
+++ static-0/videos.js
@@ -484,9 +484,9 @@ const videos = [
 		cc:"I'm not lazy. I'm currently very busy traveling Mach 1 around the center of the Earth, over 100, 000 kilometers per hour around the Sun, 828,000 kilometers per hour around the Milky Way, 2.1 million kilometers per hour through the Universe, producing over 2000 red blood cells every millisecond, shedding 56,000 skin cells every second, pumping 5 liters of blood every minute, inhaling 315 liters of air every hour, growing my hair a collective 35 meters every day, processing the equivalent of 2.6 million books every week, moving my eyes 7.2 million times every month, and digesting 600 kilograms of food every year, just to list a few things. So basically, in short...",
 		youtubeID:"N3YC7PHtGvw",
 		date:"15-7-2025",
-		views10:-1,
-		views30:-1,
-		views1:-1,
-		views24:-1,
+		views10:320,
+		views30:674,
+		views1:875,
+		views24:24507,
 	},
 ];
