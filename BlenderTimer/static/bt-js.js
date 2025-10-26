f3421c4bb3e15958b4840e4c358391d399417f7d
Daniel Roberts <inquiries@blendertimer.com>
2025-10-25 18:04:58 -0700

    Fix duplicate website and add donor

--- BlenderTimer/static/bt-js.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('btjs-download').innerHTML = "Download BT-JS " + btjsVersion;
-document.getElementById('change-log').innerHTML = btjsVersion + " Change Log";
-
-function link(l) {
-	window.location.assign(l)
-}
