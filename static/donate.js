de8b58593542b913b0829579704540ba599df9dd
Daniel Roberts <inquiries@blendertimer.com>
2026-03-06 10:46:27 -0800

    Update donate.js

--- static/donate.js
+++ static/donate.js
@@ -1,14 +1,17 @@
 const paypalDonate = document.getElementById('paypal-donate');
+let url = new URL(window.location.href);
+if (url.searchParams.get('p')) {
+	paypalDonate.href = `https://www.paypal.com/donate?business=inquiries@blendertimer.com&no_recurring=0&item_name=${url.searchParams.get('p').replace(/ /g, "+")}&amount=10`;
+}
 
 function setPaypalDonation(e) {
 	let el = e.target;
-	let url = new URL(window.location.href);
 	if (el.children.length > 1) {
-		if (url.get('p')) {
-			paypalDonate.href = `https://www.paypal.com/donate?business=inquiries@blendertimer.com&no_recurring=0&item_name=${url.get('p')}&amount=${el.children[1].innerHTML.replace("$", "")}`;
+		if (url.searchParams.get('p')) {
+			paypalDonate.href = `https://www.paypal.com/donate?business=inquiries@blendertimer.com&no_recurring=0&item_name=${url.searchParams.get('p').replace(/ /g, "+")}&amount=${el.children[1].innerHTML.replace("$", "")}`;
 		}
 		else {
-			paypalDonate.href = `https://www.paypal.com/donate?business=inquiries@blendertimer.com&no_recurring=0&item_name=BlenderTimer+Donation&amount=${{el.children[1].innerHTML.replace("$", "")}`;
+			paypalDonate.href = `https://www.paypal.com/donate?business=inquiries@blendertimer.com&no_recurring=0&item_name=BlenderTimer+Donation&amount=${el.children[1].innerHTML.replace("$", "")}`;
 		}
 	}
 }
